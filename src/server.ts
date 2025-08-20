9c6d4a52e54ce57939bba62b981cd38f20eb3a36
Rodolfo Walter <dolfo.w@gmail.com>
2025-08-20 13:44:03 -0300

    adicionada mensagem de erro na criação do filme

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,37 @@ app.get('/movies', async (_, res) => {
    include: { genres: true, languages: true },
  });
  res.json(movies);
+ 
 });
+app.post('/movies', async (req, res) => {
+console.log(`conteudo: ${req.body.title}`)
+/* const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+ await prisma.movie.create({
+   data: {
+     title: title,
+     genre_id: genre_id,
+     language_id: language_id,
+     oscar_count: oscar_count,
+     release_date: new Date(release_date),
+   },
+ });*/
+
+ res.status(201).send();
+});
+/*app.post('/movies', async (req, res) => {
+ await prisma.movie.create({
+   data: {
+     title: 'Filme de teste',
+     genre_id: 7,
+     language_id: 1,
+     oscar_count: 0,
+  // cuidado aqui, o mes começa em 0 e vai até 11
+     release_date: new Date(2020, 0, 1),
+   },
+ });
+ res.status(201).send();
+});*/
 
 app.listen(port, () => {
    console.log(`Servidor em execução em http://localhost:${port}`);
