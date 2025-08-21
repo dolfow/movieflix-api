a614101c32d9e20c2ecdda59e467cf8909345cbe
Rodolfo Walter <dolfo.w@gmail.com>
2025-08-21 14:08:21 -0300

    adicionada atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -60,6 +60,29 @@ app.post("/movies", async (req, res) => {
  res.status(201).send();
 });*/
 
+app.put("/movies/:id", async (req, res) => {
+
+try{
+   const movie = await prisma.movie.findUnique({
+     where: { id },
+   });
+
+   if (!movie) {
+     return res.status(404).send({ message: "Filme não encontrado" });
+   }
+
+   const data = { ...req.body };
+   data.release_date = data.release_date
+     ? new Date(data.release_date)
+     : undefined;
+
+     await prisma.movie.update({ where: { id }, data });
+  }catch(error){
+   return res.status(500).send({ message: "Falha ao atualizar o registro" });
+  }
+
+});
+
 app.listen(port, () => {
    console.log(`Servidor em execução em http://localhost:${port}`);
 });
